24c6349168b6a885fce44fb7e25c0b69db759562
pitbaum <baumpit25@gmail.com>
2022-10-03 08:21:47 +0200

    week 2

---
+++ gym/rrrrr/rrrr.py
@@ -0,0 +1,16 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers import Adam
+
+def build_model(states, actions):
+    model = Sequential()
+    model.add(Flatten(input_shape=(1,states)))
+    model.add(Dense(24, activation='relu'))
+    model.add(Dense(24, activation='relu'))
+    model.add(Dense(actions, activation='linear'))
+    return model
+
+model = build_model(states, actions)
+
+model.summary()
--- gym/test1.py
+++ gym/test1.py
@@ -1,6 +1,7 @@
 import gym
 import matplotlib.pyplot as plt 
-import time 
+import time
+import NNAgent
 
 #Render mode already given in the make process.
 env = gym.make('MountainCar-v0', render_mode = "human")
@@ -12,7 +13,7 @@ obs = env.reset()
 
 for step in range(num_steps):
     # take random action
-    # action = agent_function(obs) 
+    #action = agent.act(obs)
     action = env.action_space.sample()
     
     # apply the action
@@ -20,7 +21,6 @@ for step in range(num_steps):
 
     # Wait a bit before the next frame unless you want to see a crazy fast video
     time.sleep(0.001)
-    
     # If the epsiode is up, then start another one
     if done:
         env.reset()
