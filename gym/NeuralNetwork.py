95880e7401affc0fff136069e94f1d964e79b090
pitbaum <baumpit25@gmail.com>
2022-11-14 09:54:26 +0100

    magic hands

--- gym/NeuralNetwork.py
+++ gym/NeuralNetwork.py
@@ -15,10 +15,10 @@ class NeuralNetwork(nn.Module):
         # Linear network with Tanh, 2 input 1 output
         # No grad and no bias
         self.linear_network = nn.Sequential(
-            nn.Linear(2, 32, False),
-            nn.Linear(32, 16, False),
+            nn.Linear(2, 16, False),
+            nn.ReLU(),
             nn.Linear(16, 1, False),
-            nn.ReLU()
+            nn.Tanh()
         )
         # Freeze the learning of the newtork by deactivating gradient
         # Iterate over all parameters in the model and deactivate gradient
@@ -26,23 +26,23 @@ class NeuralNetwork(nn.Module):
             param.requires_grad = False
             # takes in a module and applies the specified weight initialization
 
-        self.linear_network.apply(weights_init_uniform_rule)
+        #self.linear_network.apply(weights_init_uniform_rule)
 
     # Forward function through the network
     def forward(self, x):
         # Pass observation through forward function
         # Since the observation also contains other values, only take the value index at 0
-        logits = torch.clamp(self.linear_network(torch.tensor(x[0])), min=0, max=2)
+        logits = self.linear_network(torch.tensor(x[0]))
         # Return normal python float value instead of tensor
-        floatValue = logits.item() * 100.0
+        floatValue = logits.item()
         #print(floatValue)
-        if (floatValue >= 0 and floatValue < 0.75):
-            floatValue = 1
+        if (floatValue >= -0.33 and floatValue <= 0.33):
+            floatValue = 0
         else:
-            if (floatValue >= 0.75 and floatValue < 1.25):
+            if (floatValue > 0.33):
                 floatValue = 2
             else:
-                floatValue = 0
+                floatValue = 1
         return floatValue
 
     # Set weigths and bias to given parameter from parent
@@ -124,7 +124,7 @@ class Evolution:
         return (inheritance_boolean_list_child_1, inheritance_boolean_list_child_2)
 
 
-p1 = Evolution(150, 500, 50)
+p1 = Evolution(150, 200, 50)
 
 
 # Get a better random distribution at the beginning
